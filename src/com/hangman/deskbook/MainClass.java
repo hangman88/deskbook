00105c4247ea2aacd459da04a5c9452d024d3053
hangman88 <nastavnick@gmail.com>
2022-09-28 15:03:46 +0200

    Перша проба

---
+++ src/com/hangman/deskbook/MainClass.java
@@ -0,0 +1,9 @@
+package com.hangman.deskbook;
+
+public class MainClass {
+
+    public void startMain() {
+        System.out.println("Я зробив щось.");
+    }
+
+}
---
+++ src/com/hangman/deskbook/Start.java
@@ -0,0 +1,16 @@
+package com.hangman.deskbook;
+
+
+
+
+
+public class Start {
+    public static void main(String[] args) {
+
+
+        MainClass somestupidname = new MainClass(); //створив новий класс MainClass з назвою somestupidname
+        somestupidname.startMain(); //запустив startMain у новостворенному MainClass з назвою somestupidname
+
+
+    }
+}
