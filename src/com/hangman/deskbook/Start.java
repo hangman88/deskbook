652fb63f0d88a03f8331de536e8c5f5d4261a9f5
hangman88 <nastavnick@gmail.com>
2022-09-28 15:07:57 +0200

    кемелкейс змінив

--- src/com/hangman/deskbook/Start.java
+++ src/com/hangman/deskbook/Start.java
@@ -8,8 +8,8 @@ public class Start {
     public static void main(String[] args) {
 
 
-        MainClass somestupidname = new MainClass(); //створив новий класс MainClass з назвою somestupidname
-        somestupidname.startMain(); //запустив startMain у новостворенному MainClass з назвою somestupidname
+        MainClass someStupidName = new MainClass(); //створив новий класс MainClass з назвою somestupidname
+        someStupidName.startMain(); //запустив startMain у новостворенному MainClass з назвою somestupidname
 
 
     }
